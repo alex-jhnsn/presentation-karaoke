3e31323fbf6c6387bb34c6ccf8a3b7c64709f3f2
Alex Johnson <alextheviking@hotmail.co.uk>
2021-06-18 15:08:24 +0100

    Adds working image controls

--- src/Controls.js
+++ src/Controls.js
@@ -3,11 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 function Controls(props) {
+
     return (
         <div className="controls">
-            <FontAwesomeIcon className="icon" icon={faCaretLeft} />
+            <button onClick={() => props.changeSlide(props.currentSlide-1)} disabled={props.currentSlide === 1} >
+                <FontAwesomeIcon className="icon" icon={faCaretLeft} />
+            </button>
             <p>{props.currentSlide} / {props.totalSlides}</p>
-            <FontAwesomeIcon className="icon" icon={faCaretRight} />
+            <button onClick={() => props.changeSlide(props.currentSlide+1)} disabled={props.currentSlide === props.totalSlides}>
+                <FontAwesomeIcon className="icon" icon={faCaretRight} />
+            </button>
         </div>
     );
 }
