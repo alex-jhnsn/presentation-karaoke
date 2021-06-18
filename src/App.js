189a96004819c9650889be83eeddcc348b00f0e6
Alex Johnson <alextheviking@hotmail.co.uk>
2021-06-18 15:08:01 +0100

    Adds icon to top bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './css/App.scss';
 import Slides from './Slides';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Presentation Karaoke</h1>
-        
+        <h1>Alex's Presentation Karaoke</h1>
+        <FontAwesomeIcon icon={faInfoCircle} className="icon" />
       </header>
       <div>
         <Slides/>
