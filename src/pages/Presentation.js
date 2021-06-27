bc664038d26af17bd9505833394f3e30d044ee85
Alex Johnson <alextheviking@hotmail.co.uk>
2021-06-27 12:37:50 +0100

    Adds slides changing on a timer

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -1,13 +1,20 @@
 import '../css/App.scss';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
 import Slides from '../components/Slides';
 import Header from '../components/Header';
 
 function Presentation() {
+
+  const { search } = useLocation();
+  const values = queryString.parse(search);
+
   return (
     <div className="App">
       <Header/>
       <div>
-        <Slides/>
+        
+        <Slides slides={values.slides} timer={values.timer} />
       </div>
     </div>
   );
