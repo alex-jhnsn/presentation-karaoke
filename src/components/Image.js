c18f6022db7e2577af1dcafcd5c5b8afcb6cb1cc
Alex Johnson <alextheviking@hotmail.co.uk>
2021-07-26 21:23:19 +0100

    Moves image attribution

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,11 +5,11 @@ import { faCamera } from '@fortawesome/free-solid-svg-icons';
 function Image(props) {
     return (
         <div className="image">
-            <img src={props.image.url} alt={props.image.description}/>
             <div className="attribution">
                 <FontAwesomeIcon icon={faCamera} className="icon" />
                 <a href={`https://unsplash.com/@${props.image.author.username}?utm_source=presentation-karaoke&utm_medium=referral`}>{props.image.author.name}</a> on <a href="https://unsplash.com/?utm_source=presentation-karaoke&utm_medium=referral">Unsplash</a>
             </div>
+            <img src={props.image.url} alt={props.image.description}/>
         </div>
     );
 };
